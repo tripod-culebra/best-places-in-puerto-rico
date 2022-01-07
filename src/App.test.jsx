e259f06e82fc9e5feb4ca005a8f27e45b184c785
Chris Beresford <cberesfochrisb@gmail.com>
2022-01-07 16:30:01 -0400

    refactor: app.test.js to app.test.jsx

---
+++ src/App.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import App from './App.jsx';
+
+test('renders Auth Sign In for Create Account', async () => {
+    render(<App />);
+    await waitFor(() => {
+        const signInElement = screen.queryByText(/Create Account/i);
+        expect(signInElement).toBeInTheDocument();
+    });
+});
