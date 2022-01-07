6c6c32f9223e1c885f8993dbf0997e53910cbe97
Chris Beresford <cberesfochrisb@gmail.com>
2022-01-07 16:00:49 -0400

    fix: change app.css to index.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
-import './App.css';
+import './index.css';
 import PropTypes from 'prop-types';
 import awsconfig from './aws-exports.js';
 
