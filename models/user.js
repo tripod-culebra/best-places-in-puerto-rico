c2cb529c53342cc5bf2dbab99f945d34c57dd5e1
Chris Beresford <cberesfochrisb@gmail.com>
2022-01-11 10:11:53 -0400

    feat: add user.js

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const Session = new Schema({
+    refreshToken: {
+        type: String,
+        default: '',
+    },
+});
+
+const User = new Schema({
+    firstName: {
+        type: String,
+        default: '',
+    },
+    lastName: {
+        type: String,
+        default: '',
+    },
+    authStrategy: {
+        type: String,
+        default: 'local',
+    },
+    points: {
+        type: Number,
+        default: 50,
+    },
+    refreshToken: {
+        type: [Session],
+    },
+});
+
+// Remove refreshToken from the response
+User.set('toJSON', {
+    transform(doc, ret, options) {
+        delete ret.refreshToken;
+        return ret;
+    },
+});
+
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', User);
