bc4d3fc51550f90f8c2e5df65a735efa15332175
Chris Beresford <cberesfochrisb@gmail.com>
2022-01-12 14:18:21 -0400

    refactor: add auth0 to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
 
-const App = () => (
-    <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
-        </header>
-    </div>
-);
+const App = () => {
+    const { isLoading } = useAuth0();
+
+    if (isLoading) return <div>Loading...</div>;
+
+    return (
+        <>
+            <LoginButton />
+            <LogoutButton />
+            <Profile />
+        </>
+    );
+};
 
 export default App;
