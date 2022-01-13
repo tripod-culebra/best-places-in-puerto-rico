f2ebde791e525b2f53abefcb8da7b420ad4773ac
Chris Beresford <cberesfochrisb@gmail.com>
2022-01-12 22:02:53 -0400

    feat: add profile.js

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,18 +3,18 @@ import { useAuth0 } from '@auth0/auth0-react';
 // import JSONPretty from 'react-json-pretty';
 
 const Profile = () => {
-    const { isAuthenticated, isLoading } = useAuth0();
+    const { isAuthenticated, isLoading, user } = useAuth0();
     if (isLoading) {
         return <div>Loading...</div>;
     }
     return (
-        <div>
-            {isAuthenticated ? (
-                <p>Login profile test is logged in</p>
-            ) : (
-                <p>You are not logged into profile</p>
-            )}
-        </div>
+        isAuthenticated && (
+            <div>
+                <img src={user.picture} alt={user.name} />
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+            </div>
+        )
     );
 };
 
