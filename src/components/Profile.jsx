4f887b6f52c6c13f6f80110dce8464213fd60ef6
Chris Beresford <cberesfochrisb@gmail.com>
2022-01-13 13:44:16 -0400

    fix: changed div to fragment in profile.js

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,11 +5,11 @@ const Profile = () => {
     const { user } = useAuth0();
 
     return (
-        <div>
+        <>
             <img src={user.picture} alt={user.name} />
             <h2>{user.name}</h2>
             <p>{user.email}</p>
-        </div>
+        </>
     );
 };
 
