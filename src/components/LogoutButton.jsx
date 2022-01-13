e9cf98b542ae0234f52d72665b40277a25367606
Chris Beresford <cberesfochrisb@gmail.com>
2022-01-12 22:02:22 -0400

    feat: add logoutbutton

--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const LogoutButton = () => {
-    const { logout, isLoading } = useAuth0();
+    const { logout, isLoading, isAuthenticated } = useAuth0();
 
     if (isLoading) return <div>Loading...</div>;
 
     return (
-        <button type="button" onClick={() => logout()}>
-            Log Out
-        </button>
+        isAuthenticated && (
+            <button type="button" onClick={() => logout()}>
+                Log Out
+            </button>
+        )
     );
 };
 
