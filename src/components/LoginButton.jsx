1acad805649db4961d38c375325553dd9aa57134
Chris Beresford <cberesfochrisb@gmail.com>
2022-01-13 11:56:02 -0400

    refactor: LoginButton.js

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const LoginButton = () => {
-    const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
+    const { loginWithRedirect } = useAuth0();
 
-    if (isLoading) return <div>Loading...</div>;
     return (
-        !isAuthenticated && (
-            <button type="button" onClick={() => loginWithRedirect()}>
-                Log In
-            </button>
-        )
+        <button type="button" onClick={() => loginWithRedirect()}>
+            Log In
+        </button>
     );
 };
 
