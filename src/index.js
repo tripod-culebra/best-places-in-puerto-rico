99dac5c44a4013f459302022781a1056152040df
Chris Beresford <cberesfochrisb@gmail.com>
2022-01-12 22:07:45 -0400

    refactor: index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { BrowserRouter as Router } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
-// import Auth0ProviderWithHistory from './auth/auth0-provider-with-history';
+import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
@@ -19,4 +17,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
