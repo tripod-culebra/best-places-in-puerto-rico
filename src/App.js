0cfe8a3f0793c91da6c50f3b669c9a028df92d48
Chris Beresford <cberesfochrisb@gmail.com>
2022-01-13 20:51:11 -0400

    style: add background image on login

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
 import Profile from './components/Profile';
+import background from './PRimage.jpeg';
 
 const App = () => {
     const { error, isAuthenticated, isLoading } = useAuth0();
@@ -15,7 +16,10 @@ const App = () => {
     }
 
     return !isAuthenticated ? (
-        <LoginButton />
+        <>
+            <img src={background} id="bg" alt="" />
+            <LoginButton />
+        </>
     ) : (
         <>
             <LogoutButton />
--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,13 +1,19 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import background from '../PRimage.jpeg';
 
 const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
 
     return (
-        <button type="button" onClick={() => loginWithRedirect()}>
-            Log In
-        </button>
+        <>
+            <img src={background} id="bg" />
+            <button type="button" onClick={() => loginWithRedirect()}>
+                Log In
+            </button>
+            {/* <div style={{ backgroundImage: `url(${background})` }} /> */}
+        </>
     );
 };
 
