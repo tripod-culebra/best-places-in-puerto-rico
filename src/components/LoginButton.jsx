68d4542558489969e7e85d6e639cbb94f9e95ee2
Chris Beresford <cberesfochrisb@gmail.com>
2022-01-13 20:51:51 -0400

    fix: drop background image - now on app.js

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,19 +1,13 @@
-/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import background from '../PRimage.jpeg';
 
 const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
 
     return (
-        <>
-            <img src={background} id="bg" />
-            <button type="button" onClick={() => loginWithRedirect()}>
-                Log In
-            </button>
-            {/* <div style={{ backgroundImage: `url(${background})` }} /> */}
-        </>
+        <button type="button" onClick={() => loginWithRedirect()}>
+            Log In
+        </button>
     );
 };
 
