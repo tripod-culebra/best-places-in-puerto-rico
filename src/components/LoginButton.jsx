817a3044461e3a8779d4da77ea1939ea8db3e30e
Chris Beresford <cberesfochrisb@gmail.com>
2022-01-13 21:51:03 -0400

    refactor: add react module css

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import ButtonCSS from './Button.module.css';
 
 const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
 
     return (
-        <button type="button" onClick={() => loginWithRedirect()}>
+        <button type="button" className={ButtonCSS.login} onClick={() => loginWithRedirect()}>
             Log In
         </button>
     );
