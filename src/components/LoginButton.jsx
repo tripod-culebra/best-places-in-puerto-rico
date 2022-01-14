9ad33c77d2556788a2ad1ab7c3e95cc6bc31a31b
Chris Beresford <cberesfochrisb@gmail.com>
2022-01-14 11:22:11 -0400

    fix: naming convention for css

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import ButtonCSS from './Button.module.css';
+import styles from './button.module.css';
 
 const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
 
     return (
-        <button type="button" className={ButtonCSS.login} onClick={() => loginWithRedirect()}>
+        <button type="button" className={styles.login} onClick={() => loginWithRedirect()}>
             Log In
         </button>
     );
