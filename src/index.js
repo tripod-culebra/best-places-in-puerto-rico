cd95623a18cb8d01abb4b7ce05812005db048115
Chris Beresford <cberesfochrisb@gmail.com>
2022-01-14 11:21:08 -0400

    refactor: drop index css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
