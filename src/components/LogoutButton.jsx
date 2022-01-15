cab98b2b17cf791f5895fd93da4219ae2d27738d
Chris Beresford <cberesfochrisb@gmail.com>
2022-01-14 21:06:00 -0400

    style: add style module

--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import styles from '../styles/button.module.css';
 
 const LogoutButton = () => {
     const { logout } = useAuth0();
 
     return (
-        <button type="button" onClick={() => logout()}>
+        <button type="button" className={styles.logout} onClick={() => logout()}>
             Log Out!
         </button>
     );
