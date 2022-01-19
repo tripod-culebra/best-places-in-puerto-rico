1c6c9febf6566d45c0add3954e409ddbf0de8489
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-01-19 19:45:23 -0400

    fix: add className css src root identifier

--- src/components/pages/PlacesIWantToGo/index.jsx
+++ src/components/pages/PlacesIWantToGo/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import backGroundImage from '../../../assets/PRArecibo.jpeg';
-import styles from '../../../index.module.css';
+import backgroundImage from '../../../assets/PRArecibo.jpeg';
 
 const PlacesIWantToGo = () => (
     <>
-        <img src={backGroundImage} className={styles.background} alt="Arecibo" />
-        <h1 className={styles.header}>Places I Want To Go!</h1>
+        <img src={backgroundImage} className="background" alt="Arecibo" />
+        <h1 className="header">Places I Want To Go!</h1>
     </>
 );
 export default PlacesIWantToGo;
