0d2c404a413b42c53186c22863e9dafce1729d38
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-01-19 12:09:15 -0400

    feat: add navbar.index.jsx

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+// import styles from '../../styles/button.module.css';
+import styles from './index.module.css';
+
+const Navbar = () => {
+    const navigate = useNavigate();
+    const { logout } = useAuth0();
+
+    return (
+        <>
+            <button type="button" className={styles.home} onClick={() => navigate('/')}>
+                Home
+            </button>
+            <button
+                type="button"
+                className={styles.places}
+                onClick={() => navigate('/PlacesIWantToGo')}
+            >
+                Places I Want To Go!!
+            </button>
+            <button
+                type="button"
+                className={styles.places}
+                onClick={() => navigate('/PlacesIHaveBeen')}
+            >
+                Places I Have Been!!
+            </button>
+            <button type="button" className={styles.logout} onClick={() => logout()}>
+                Log Out!
+            </button>
+        </>
+    );
+};
+
+export default Navbar;
