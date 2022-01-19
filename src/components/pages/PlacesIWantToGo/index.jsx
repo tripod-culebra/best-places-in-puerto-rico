2618e17edab1c435ffc7d32b1344370d73873da4
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-01-19 15:42:33 -0400

    fix: now only one css import

--- src/components/pages/PlacesIWantToGo/index.jsx
+++ src/components/pages/PlacesIWantToGo/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import backGroundImage from '../../../assets/PR_Arecibo.jpeg';
-import imageStyles from '../../index.module.css';
-import pageStyles from '../index.module.css';
+import backGroundImage from '../../../assets/PRArecibo.jpeg';
+import styles from '../../../index.module.css';
 
 const PlacesIWantToGo = () => (
     <>
-        <img src={backGroundImage} className={imageStyles.backGround} alt="" />
-        <h1 className={pageStyles.header}>Places I Want To Go!</h1>
+        <img src={backGroundImage} className={styles.background} alt="Arecibo" />
+        <h1 className={styles.header}>Places I Want To Go!</h1>
     </>
 );
 export default PlacesIWantToGo;
