f0e72c751bddbb678cd58e9e0b25893a5fd68ea3
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-01-19 12:02:56 -0400

    refactor: imports  Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import styles from './styles/backGroundImages.module.css';
+import styles from './components/index.module.css';
 import LoginButton from './components/LoginButton';
-import LogoutButton from './components/LogoutButton';
 import backGroundImage from './assets/PR_Cabo_Rojo.jpeg';
-import Home from './components/Home';
-import PlacesIWantToGo from './components/PlacesIWantToGo';
-import PlacesIHaveBeen from './components/PlacesIHaveBeen';
+import Home from './components/pages/Home';
+import PlacesIWantToGo from './components/pages/PlacesIWantToGo';
+import PlacesIHaveBeen from './components/pages/PlacesIHaveBeen';
+import Navbar from './components/navbar';
 
 const App = () => {
     const { error, isAuthenticated, isLoading } = useAuth0();
@@ -29,8 +29,9 @@ const App = () => {
                 <Route exact path="/" element={<Home />} />
                 <Route path="/PlacesIWantToGo" element={<PlacesIWantToGo />} />
                 <Route path="/PlacesIHaveBeen" element={<PlacesIHaveBeen />} />
-                <Route path="/Logout" element={<LogoutButton />} />
+                <Route path="/Logout" element={<Navbar />} />
             </Routes>
+            <Navbar />
         </Router>
     );
 };
