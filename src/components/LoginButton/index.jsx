91ec591a6501033f678abdb497dfc325d9516c7f
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-01-19 12:05:59 -0400

    feat: LoginButton index.jsx

---
+++ src/components/LoginButton/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+// import styles from '../../styles/button.module.css';
+import styles from './index.module.css';
+
+const LoginButton = () => {
+    const { loginWithRedirect } = useAuth0();
+
+    return (
+        <button type="button" className={styles.login} onClick={() => loginWithRedirect()}>
+            Log In To Plan Your Next Puerto Rican Adventure!
+        </button>
+    );
+};
+
+export default LoginButton;
