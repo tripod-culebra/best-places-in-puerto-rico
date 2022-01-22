786ed3d1c0880c398b4692edfc5bdf2e4f2c62c5
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-01-22 15:04:30 -0400

    refactor: destructured css

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -6,27 +6,24 @@ import styles from './index.module.css';
 const Navbar = () => {
     const navigate = useNavigate();
     const { logout } = useAuth0();
+    const { home, placesBeen, placesGo, logoutCSS } = styles;
 
     return (
         <nav>
-            <button type="button" className={styles.home} onClick={() => navigate('/')}>
+            <button type="button" className={home} onClick={() => navigate('/')}>
                 Home
             </button>
-            <button
-                type="button"
-                className={styles.placesGo}
-                onClick={() => navigate('/PlacesIWantToGo')}
-            >
+            <button type="button" className={placesGo} onClick={() => navigate('/PlacesIWantToGo')}>
                 Places I Want To Go!!
             </button>
             <button
                 type="button"
-                className={styles.placesBeen}
+                className={placesBeen}
                 onClick={() => navigate('/PlacesIHaveBeen')}
             >
                 Places I Have Been!!
             </button>
-            <button type="button" className={styles.logout} onClick={logout}>
+            <button type="button" className={logoutCSS} onClick={logout}>
                 Log Out!
             </button>
         </nav>
