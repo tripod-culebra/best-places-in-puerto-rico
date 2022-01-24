80813438139ca638eed335e0601c5e8964c1688b
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-01-24 19:06:36 -0400

    feat: add server index.js

---
+++ server/index.js
@@ -0,0 +1,14 @@
+// const path = require('path');
+// const dotenv = require('dotenv');
+
+// dotenv.config({
+//     path: path.resolve(__dirname, '../.env'),
+// });
+
+const { app } = require('./app');
+
+const PORT = 3000 || 8080;
+
+app.listen(PORT, () => {
+    console.info(`Server listening on :${PORT}`);
+});
