c07b4d298711f8c0f1c7cc65c7a29d641b81e1c3
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-01-24 21:49:14 -0400

    fix: drop fake data

--- src/components/pages/PlacesIHaveBeen/index.jsx
+++ src/components/pages/PlacesIHaveBeen/index.jsx
@@ -1,56 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import backgroundImage from '../../../assets/PROceanPark.jpeg';
 import PlacesBeenForm from '../../forms/PlacesBeenForm';
 
-const data = [
-    {
-        id: 23423,
-        places: 'la parguera',
-        description: 'best boating place',
-        what: 'take small boat to man groves',
-        when: '1, 1, 2022',
-        who: 'ali',
-        rating: 5,
-    },
-    {
-        id: 23424,
-        places: 'culebra',
-        description: 'best small island nearby',
-        what: 'small island with stellar beaches',
-        when: '12, 15, 2021',
-        who: 'sofia',
-        rating: 5,
-    },
-    {
-        id: 23425,
-        places: 'cabo rojo',
-        description: 'best sunset on the island',
-        what: 'calm waters and always sunny',
-        when: '10, 5, 2021',
-        who: 'ali and sofia',
-        rating: 5,
-    },
-];
-
-const PlacesIHaveBeen = () => (
-    <div>
-        <img src={backgroundImage} className="background" alt="Ocean Park" />
-        <h1 className="header">Places I Have Been!</h1>
-        <div>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Places I Have Visted</th>
-                        <th>Description of Location</th>
-                        <th>What I Did</th>
-                        <th>When I Went</th>
-                        <th>Who Went?</th>
-                        <th>Rating</th>
-                    </tr>
-                </thead>
-                {data.map(val => {
-                    const { places, description, what, when, who, rating, id } = val;
-                    return (
+const PlacesIHaveBeen = () => {
+    const data = useState([]);
+    return (
+        <>
+            <img src={backgroundImage} className="background" alt="Ocean Park" />
+            <h1 className="header">Places I Have Been!</h1>
+            <div>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Places I Have Visted</th>
+                            <th>Description of Location</th>
+                            <th>What I Did</th>
+                            <th>When I Went</th>
+                            <th>Who Went?</th>
+                            <th>Rating</th>
+                        </tr>
+                    </thead>
+                    {data.map(({ places, description, what, when, who, rating, id }) => (
                         <tbody key={id}>
                             <tr>
                                 <td>{places}</td>
@@ -61,12 +31,12 @@ const PlacesIHaveBeen = () => (
                                 <td>{rating}</td>
                             </tr>
                         </tbody>
-                    );
-                })}
-            </table>
-        </div>
-        <PlacesBeenForm />
-    </div>
-);
+                    ))}
+                </table>
+            </div>
+            <PlacesBeenForm />
+        </>
+    );
+};
 
 export default PlacesIHaveBeen;
