1b7c78fe50707ef0ba2153b5886f9dc28b9de3ca
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-01-26 15:25:44 -0400

    fix: drop un-used dotenv

--- server/index.js
+++ server/index.js
@@ -1,10 +1,3 @@
-// const path = require('path');
-// const dotenv = require('dotenv');
-
-// dotenv.config({
-//     path: path.resolve(__dirname, '../.env'),
-// });
-
 const { app } = require('./app');
 
 const PORT = 8080;
