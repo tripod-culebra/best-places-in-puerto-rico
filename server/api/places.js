2e92238041d30354343b7c00df551d2f958cec2f
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-01-25 21:23:50 -0400

    feat: add get api

--- server/api/places.js
+++ server/api/places.js
@@ -1,17 +1,30 @@
 const { Router } = require('express');
 const db = require('../db/index');
+// const PlacesGoForm = require('../../src/components/forms/PlacesGoForm/index');
 
 const Places = Router();
-Places.post('/PlacesIWantToGo', (req, res) =>
+Places.post('/', (req, res) =>
     db
-        .savePlace()
+        .savePlace(req.body.formData)
         .then(results => {
-            console.info(results, 'results in placesIWantToGo api/places');
+            console.info(results, 'results in post placesIWantToGo api/places');
             res.send(results);
             res.status(201);
         })
         .catch(error => {
             res.sendStatus(500);
+            console.error(error, 'error getting repos repos.js');
+        })
+);
+Places.get('/', (req, res) =>
+    db
+        .getTop25Places(req.body.formData)
+        .then(results => {
+            console.info(results[0], 'results in get placesIWantToGo api/places');
+            res.status(201).send(results);
+        })
+        .catch(error => {
+            res.status(500).json(JSON.stringify({ results: 'Okay' }));
             console.error(error, 'error getting repos repos.js');
         })
 );
