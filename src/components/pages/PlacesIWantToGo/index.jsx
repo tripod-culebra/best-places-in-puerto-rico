9013d6ffd0e5eea9bb478bebdf4386dd1b92ad5f
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-01-26 15:28:01 -0400

    refactor: fetch to axios

--- src/components/pages/PlacesIWantToGo/index.jsx
+++ src/components/pages/PlacesIWantToGo/index.jsx
@@ -1,35 +1,14 @@
-/* eslint-disable no-shadow */
-/* eslint-disable no-console */
-/* eslint-disable react-hooks/exhaustive-deps */
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import backgroundImage from '../../../assets/PRArecibo.jpeg';
 import PlacesGoForm from '../../forms/PlacesGoForm';
 
 const PlacesIWantToGo = () => {
     const [data, setData] = useState([]);
-    const url = 'http://localhost:8080/api/places';
-    const requestOptions = {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-    };
-    // const fetchData = () =>
-    //     fetch(url, requestOptions)
-    //         .then(results => {
-    //             console.log(results.json(), 'got PlacesGo I want to go successfully');
-    //             setData([results.json()]);
-    //             console.info(data, 'DATAAAAAAAA');
-    //         })
-    //         .catch(error => console.error(error, 'error fetching data PlacesGo'));
-    // useEffect(() => {
-    //     fetchData();
-    // }, []);
     useEffect(() => {
-        fetch(url, requestOptions)
-            .then(res => res.json())
-            .then(data => {
-                console.info(data, 'what');
-                setData(data);
-            });
+        axios.get('http://localhost:8080/api/places').then(result => {
+            setData(result.data);
+        });
     }, []);
 
     return (
@@ -49,31 +28,23 @@ const PlacesIWantToGo = () => {
                             <th>Completed?</th>
                         </tr>
                     </thead>
-                    {data.map(val => {
-                        // {formData.map(val => {
-                        /* {results.map(val => { */
-                        const { places, description, what, when, who, rating, id } = val;
-                        // const { description } = val;
-                        console.info(data, 'WHAT IS THIS DESCRIPTION??');
-                        return (
-                            <tbody key={id}>
-                                <tr>
-                                    <td>{places}</td>
-                                    <td>{description}</td>
-                                    <td>{what}</td>
-                                    <td>{when}</td>
-                                    <td>{who}</td>
-                                    <td>{rating}</td>
-                                </tr>
-                            </tbody>
-                        );
-                    })}
+                    {data.map(({ place, description, what, when, who, rating, _id }) => (
+                        <tbody key={_id}>
+                            <tr>
+                                <td>{place}</td>
+                                <td>{description}</td>
+                                <td>{what}</td>
+                                <td>{when}</td>
+                                <td>{who}</td>
+                                <td>{rating}</td>
+                            </tr>
+                        </tbody>
+                    ))}
                 </table>
                 <PlacesGoForm />
             </div>
         </>
     );
-    // });
 };
 
 export default PlacesIWantToGo;
