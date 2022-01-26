30e28a4b25b39f3a59c62fd951faf7f611aa31af
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-01-25 21:21:10 -0400

    feat: add cors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const { Places } = require('./api/places');
 
 const CLIENT_PATH = path.resolve(__dirname, '../build');
@@ -8,8 +9,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(CLIENT_PATH));
-
+app.use(cors());
 app.use('/api/places', Places);
+// app.get('/api/places', (req, res) => {
+//     res.send('hello worls');
+// });
+
+// app.post('/api/places', (req, res) => {
+//     res.send('hiiiii');
+// });
 
 module.exports = {
     app,
