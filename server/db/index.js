82302ba56c1d6605e9252dcf26895ebec60b0b0d
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-01-25 21:24:18 -0400

    fix: schema

--- server/db/index.js
+++ server/db/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-return-assign */
 /* eslint-disable consistent-return */
 const mongoose = require('mongoose');
 
@@ -10,13 +11,13 @@ mongoose
     .catch(err => console.error('Failed to connect to database', err));
 
 const placesSchema = new mongoose.Schema({
-    id: Number,
-    places: String,
+    // id: Number,
+    place: String,
     description: String,
     what: String,
     when: { type: Date, default: Date.now },
-    with: String,
-    rating: Number,
+    who: String,
+    rating: String,
     completed: Boolean,
 });
 
@@ -25,22 +26,53 @@ const Place = mongoose.model('Place', placesSchema);
 const savePlace = place => {
     const savedPlace = new Place({
         id: place.id,
-        places: place.places,
+        place: place.place,
         description: place.description,
         what: place.what,
         when: place.when,
-        with: place.with,
+        who: place.who,
         rating: place.rating,
         completed: place.completed,
     });
-    return Place.find({ id: place.id }).then(results => {
-        if (results.length === 0) {
-            return savedPlace.save(place);
-        }
-    });
+    // return Place.find({ description: place.description }).then(results => {
+    // if (results.length === 0) {
+    return savedPlace.save(place);
+    // }
+    // });
 };
 
-const getTop25Places = () => Place.find({}).sort({ date: 'desc' }).limit(25);
+// const getPlaces = () =>
+//     (getPlacesPromise = new Promise((resolve, reject) => {
+//         Place.find().exec((error, results) => {
+//             if (error) {
+//                 console.error(error, 'error getting places');
+//                 reject(error);
+//             } else {
+//                 console.info(results, 'found places');
+//                 resolve(results);
+//             }
+//         });
+//     }));
+
+// const getTop25Places = () => Place.find({}).sort({ date: 'desc' }).limit(25);
+const getTop25Places = () =>
+    // eslint-disable-next-line no-undef
+    (getTop25ReposPromise = new Promise((resolve, reject) => {
+        Place.find()
+            // find returns a promise but wrap the whole in a promise to have the chain be a promise
+            .sort({ date: -1 })
+            .limit(25)
+            .exec((error, results) => {
+                if (error) {
+                    console.error(error, 'error in getting top 25 results!');
+                    reject(error);
+                } else {
+                    console.info('found top 25 results!');
+                    resolve(results);
+                }
+            });
+        // );
+    }));
 
 module.exports.savePlace = savePlace;
 module.exports.getTop25Places = getTop25Places;
