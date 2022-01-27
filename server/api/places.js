59f1c71130d181e69bad81079a8399e044e4af49
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-01-27 11:35:05 -0400

    feat: add placesBeen api

--- server/api/places.js
+++ server/api/places.js
@@ -13,13 +13,40 @@ Places.post('/', (req, res) =>
         })
         .catch(error => {
             res.sendStatus(500);
-            console.error(error, 'error getting repos repos.js');
+            console.error(error, 'error getting placesIWantToGo api/places');
+        })
+);
+
+Places.post('/been', (req, res) =>
+    db
+        .savePlace(req.body.formData)
+        .then(results => {
+            console.info(results, 'results in post placesBeen api/places');
+            res.send(results);
+            res.status(201);
+        })
+        .catch(error => {
+            res.sendStatus(500);
+            console.error(error, 'error getting placesBeen api/places');
         })
 );
 
 Places.get('/', (req, res) =>
     db
         .getTop25Places()
+        .then(results => {
+            console.info(results, 'results in get placesIWantToGo api/places');
+            res.status(201).send(results);
+        })
+        .catch(error => {
+            res.status(500).json(JSON.stringify({ results: 'Okay' }));
+            console.error(error, 'error getting repos repos.js');
+        })
+);
+
+Places.get('/been', (req, res) =>
+    db
+        .getTop25PlacesBeen()
         .then(results => {
             console.info(results, 'results in get placesIWantToGo api/places');
             res.status(201).send(results);
