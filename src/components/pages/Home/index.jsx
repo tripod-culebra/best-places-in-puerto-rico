2ce621ad33198b391ce7a3952ecb910e42bca0a0
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-01-28 11:06:33 -0400

    fix: drop import background asset

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { upperFirst } from 'lodash';
-import backgroundImage from '../../../assets/PRCondado.jpeg';
 
 const Home = () => {
     const {
         user: { given_name: givenName },
     } = useAuth0();
-    return (
-        <>
-            <img src={backgroundImage} className="background" alt="Condado" />
-            <h1 className="header">Bienvenidos a Puerto Rico {upperFirst(givenName)}!</h1>
-        </>
-    );
+    return <h1 className="header">Bienvenidos a Puerto Rico {upperFirst(givenName)}!</h1>;
 };
 
 export default Home;
