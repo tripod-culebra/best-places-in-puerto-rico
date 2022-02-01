a929e0996019282df62b73d27ea15672417695fa
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-02-01 15:20:39 -0400

    feat: add background image state

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
+import propTypes from 'prop-types';
 import styles from './index.module.css';
 
-const Navbar = () => {
+const Navbar = ({ setBackgroundImageType }) => {
     const navigate = useNavigate();
     const { logout } = useAuth0();
     const { home, placesBeen, placesGo, logoutCSS } = styles;
 
     return (
         <nav>
-            <button type="button" className={home} onClick={() => navigate('/')}>
+            <button
+                type="button"
+                className={home}
+                onClick={() => {
+                    setBackgroundImageType('default');
+                    navigate('/');
+                }}
+            >
                 Home
             </button>
-            <button type="button" className={placesGo} onClick={() => navigate('/PlacesIWantToGo')}>
+            <button
+                type="button"
+                className={placesGo}
+                onClick={() => {
+                    setBackgroundImageType('PlacesIWantToGo');
+                    navigate('/PlacesIWantToGo');
+                }}
+            >
                 Places I Want To Go!!
             </button>
             <button
                 type="button"
                 className={placesBeen}
-                onClick={() => navigate('/PlacesIHaveBeen')}
+                onClick={() => {
+                    setBackgroundImageType('PlacesIHaveBeen');
+                    navigate('/PlacesIHaveBeen');
+                }}
             >
                 Places I Have Been!!
             </button>
@@ -28,6 +46,10 @@ const Navbar = () => {
             </button>
         </nav>
     );
+};
+
+Navbar.propTypes = {
+    setBackgroundImageType: propTypes.func.isRequired,
 };
 
 export default Navbar;
