aef82baa696969dff9125f013464825d2af6a810
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-02-01 22:44:24 -0400

    fix: add correct component

--- src/components/pages/PlacesIWantToGo/index.jsx
+++ src/components/pages/PlacesIWantToGo/index.jsx
@@ -1,29 +1,30 @@
-import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import PlacesBeenForm from '../../forms/PlacesBeenForm';
+import React, { useState, useEffect } from 'react';
+import PlacesGoForm from '../../forms/PlacesGoForm';
 
 const DOMAIN = process.env.REACT_APP_DOMAIN;
 
-const PlacesIHaveBeen = () => {
+const PlacesIWantToGo = () => {
     const [data, setData] = useState([]);
     useEffect(() => {
-        axios.get(`${DOMAIN}api/places/been`).then(result => {
+        axios.get(`${DOMAIN}api/places/go`).then(result => {
             setData(result.data);
         });
     }, []);
     return (
-        <div>
-            <h1 className="header">Places I Have Been!</h1>
+        <>
+            <h1 className="header">Places I Want To Go!</h1>
             <div>
                 <table className="table">
                     <thead>
                         <tr>
-                            <th>Places I Have Visted</th>
+                            <th>Places I Want To Go To</th>
                             <th>Description of Location</th>
-                            <th>What I Did</th>
-                            <th>When I Went</th>
-                            <th>Who Went?</th>
+                            <th>What I Want To Do</th>
+                            <th>When I Want To Go</th>
+                            <th>With Who?</th>
                             <th>Rating</th>
+                            <th>Completed?</th>
                         </tr>
                     </thead>
                     {data.map(({ place, description, what, when, who, rating, _id: id }) => (
@@ -35,14 +36,17 @@ const PlacesIHaveBeen = () => {
                                 <td>{when.slice(0, 10)}</td>
                                 <td>{who}</td>
                                 <td>{rating}</td>
+                                <td>
+                                    <input type="checkbox" className="check-box" />
+                                </td>
                             </tr>
                         </tbody>
                     ))}
                 </table>
+                <PlacesGoForm setData={setData} />
             </div>
-            <PlacesBeenForm setData={setData} />
-        </div>
+        </>
     );
 };
 
-export default PlacesIHaveBeen;
+export default PlacesIWantToGo;
