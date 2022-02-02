302be76da9b093d8cb00f068f2e90894b3c4e706
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-02-02 19:24:52 -0400

    fix: 'PlacesIWantToGo' and 'PlacesIHaveBeen' to re-usable component 'Places'

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,25 +1,42 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
+import propTypes from 'prop-types';
 import styles from './index.module.css';
 
-const Navbar = () => {
+const Navbar = ({ setPlaceSelector }) => {
     const navigate = useNavigate();
     const { logout } = useAuth0();
     const { home, placesBeen, placesGo, logoutCSS } = styles;
 
     return (
         <nav>
-            <button type="button" className={home} onClick={() => navigate('/')}>
+            <button
+                type="button"
+                className={home}
+                onClick={() => {
+                    navigate('/');
+                }}
+            >
                 Home
             </button>
-            <button type="button" className={placesGo} onClick={() => navigate('/PlacesIWantToGo')}>
+            <button
+                type="button"
+                className={placesGo}
+                onClick={() => {
+                    setPlaceSelector('PlacesGo');
+                    navigate('/Places');
+                }}
+            >
                 Places I Want To Go!!
             </button>
             <button
                 type="button"
                 className={placesBeen}
-                onClick={() => navigate('/PlacesIHaveBeen')}
+                onClick={() => {
+                    setPlaceSelector('PlacesBeen');
+                    navigate('/Places');
+                }}
             >
                 Places I Have Been!!
             </button>
@@ -28,6 +45,10 @@ const Navbar = () => {
             </button>
         </nav>
     );
+};
+
+Navbar.propTypes = {
+    setPlaceSelector: propTypes.func.isRequired,
 };
 
 export default Navbar;
