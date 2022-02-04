334bc8ccc983d730be5cc838ccf65d96661e1120
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-02-04 18:15:11 -0400

    feat: add server api routing

---
+++ server/api/places.js
@@ -0,0 +1,43 @@
+const { Router } = require('express');
+const db = require('../db/index');
+
+const Places = Router();
+
+Places.post('/save', (req, res) =>
+    db
+        .savePlace(req.body.formData)
+        .then(results => {
+            res.send(results);
+            res.status(201);
+        })
+        .catch(error => {
+            res.sendStatus(500);
+            console.error(error, 'Error: Saving Place');
+        })
+);
+
+Places.get('/', (req, res) =>
+    db
+        .getTopPlacesGo()
+        .then(results => res.status(201).send(results))
+        .catch(error => {
+            res.status(500);
+            console.error(error, 'Error: Getting Places');
+        })
+);
+
+Places.put('/update', (req, res) => {
+    const changeId = Object.keys(req.body);
+    db.updatePlaceGo(changeId)
+        .then(response => {
+            res.status(201).send(response);
+        })
+        .catch(error => {
+            res.status(500);
+            console.error(error, 'Error: Updating Place I Want To Go');
+        });
+});
+
+module.exports = {
+    Places,
+};
