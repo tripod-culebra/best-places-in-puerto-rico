3bc131b23f660b8ffc52c1e485e5ebc3a942d47f
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-02-04 18:13:31 -0400

    feat: add api request and state

--- src/components/forms/PlacesGoForm/index.jsx
+++ src/components/forms/PlacesGoForm/index.jsx
@@ -1,67 +1,128 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import propTypes from 'prop-types';
 
-const PlacesGoForm = () => (
-    <div id="places-form">
-        <div>
-            <h1>Enter A Magical You Want To Visit!</h1>
-            <h4>Lets create memories!!</h4>
+const DOMAIN = process.env.REACT_APP_DOMAIN;
+
+const PlacesGoForm = ({ setData }) => {
+    const [formData, setFormData] = useState({
+        place: '',
+        description: '',
+        what: '',
+        when: '',
+        who: '',
+        rating: '',
+        completed: false,
+    });
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios({
+            method: 'post',
+            url: `${DOMAIN}/api/places/save`,
+            data: { formData },
+        })
+            .then(() => axios.get(`${DOMAIN}/api/places`).then(result => setData(result.data)))
+            .catch(error => console.error(error, 'Error: Form Not Saved'));
+    };
+
+    return (
+        <div id="places-form">
+            <div>
+                <h1>Enter A Magical You Want To Visit!</h1>
+                <h4>Lets create memories!!</h4>
+            </div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="PlacesIWantToGo">
+                        <span>Place I Want To Go To</span>
+                        <input
+                            onChange={e => setFormData({ ...formData, place: e.target.value })}
+                            value={formData.place}
+                            type="text"
+                            placeholder="Enter Place I Want To Go"
+                            id="PlacesIWantToGo"
+                            required
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label htmlFor="description">
+                        <span>Description Of Location</span>
+                        <textarea
+                            onChange={e =>
+                                setFormData({ ...formData, description: e.target.value })
+                            }
+                            value={formData.description}
+                            placeholder="Enter Desctiption"
+                            id="description"
+                            required
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label htmlFor="what">
+                        <span>What Do You Want To Do?</span>
+                        <textarea
+                            onChange={e => setFormData({ ...formData, what: e.target.value })}
+                            value={formData.what}
+                            placeholder="Enter What You Want To Do"
+                            id="what"
+                            required
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label htmlFor="when">
+                        <span>When Do You Want To Go?</span>
+                        <input
+                            onChange={e => setFormData({ ...formData, when: e.target.value })}
+                            value={formData.when}
+                            type="date"
+                            id="when"
+                            required
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label htmlFor="who">
+                        <span>Who Do You Want To Go?</span>
+                        <textarea
+                            onChange={e => setFormData({ ...formData, who: e.target.value })}
+                            value={formData.who}
+                            placeholder="Enter Who Went"
+                            id="who"
+                            required
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label htmlFor="rating">
+                        <span className="required">Enter A Rating From 1-5</span>
+                        <input
+                            onChange={e => setFormData({ ...formData, rating: e.target.value })}
+                            value={formData.rating}
+                            type="number"
+                            id="rating"
+                            min="1"
+                            max="5"
+                            placeholder="Enter Rating"
+                            required
+                        />
+                    </label>
+                </div>
+                <div>
+                    <button name="submit" type="submit">
+                        SAVE WHERE TO GO
+                    </button>
+                </div>
+            </form>
         </div>
-        <form>
-            <div>
-                <label htmlFor="placesGo">
-                    <span>Place I Want To Go To</span>
-                    <input
-                        type="text"
-                        placeholder="Enter Place I Want To Go"
-                        id="placesGo"
-                        required
-                    />
-                </label>
-            </div>
-            <div>
-                <label htmlFor="descriptionGo">
-                    <span>Description Of Location</span>
-                    <textarea placeholder="Enter Desctiption" id="descriptionGo" required />
-                </label>
-            </div>
-            <div>
-                <label htmlFor="whatGo">
-                    <span>What Do You Want To Do?</span>
-                    <textarea placeholder="Enter What You Want To Do" id="whatGo" required />
-                </label>
-            </div>
-            <div>
-                <label htmlFor="whenGo">
-                    <span>When Do You Want To Go?</span>
-                    <input type="date" id="whenGo" required />
-                </label>
-            </div>
-            <div>
-                <label htmlFor="whoGo">
-                    <span>Who Do You Want To Go?</span>
-                    <textarea placeholder="Enter Who Went" id="whoGo" required />
-                </label>
-            </div>
-            <div>
-                <label htmlFor="ratingGo">
-                    <span className="required">Enter A Rating From 1-5</span>
-                    <input
-                        type="number"
-                        id="ratingGo"
-                        min="1"
-                        max="5"
-                        placeholder="Enter Rating"
-                        required
-                    />
-                </label>
-            </div>
-            <div>
-                <button name="submit" type="submit" id="submitGo">
-                    SAVE WHERE TO GO
-                </button>
-            </div>
-        </form>
-    </div>
-);
+    );
+};
+
+PlacesGoForm.propTypes = {
+    setData: propTypes.func.isRequired,
+};
 
 export default PlacesGoForm;
