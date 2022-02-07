49a5450dc2e0f76b5b5a771c3e55dc54269cb47e
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-02-07 19:12:32 -0400

    fix: end of line

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,13 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "bestplacesinpre40756a8": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    }
-}
+    auth: {
+        bestplacesinpre40756a8: {
+            IdentityPoolId: 'string';
+            IdentityPoolName: 'string';
+            UserPoolId: 'string';
+            UserPoolArn: 'string';
+            UserPoolName: 'string';
+            AppClientIDWeb: 'string';
+            AppClientID: 'string';
+        };
+    };
+};
