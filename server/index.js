ee5fbda8bf1860355ac359d9d02c70ffae2e572c
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-02-08 14:13:05 -0400

    refactor: best practice dotenv

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const path = require('path');
-const dotenv = require('dotenv');
+// const path = require('path');
+// const dotenv = require('dotenv');
+require('dotenv').config();
 
-dotenv.config({
-    path: path.resolve(__dirname, '../.env'),
-});
-
+// dotenv.config({
+//     path: path.resolve(__dirname, '../.env'),
+// });
 const { app } = require('./app');
 
 const { PORT } = process.env;
 
-app.listen(PORT, () => console.info(`Server listening on :${PORT}`));
+app.listen(PORT, () => console.info(`Server listening on : ${PORT}`));
