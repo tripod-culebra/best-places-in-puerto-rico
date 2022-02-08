a59fe3913100338b192a438d3ec671232977119f
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-02-08 14:15:56 -0400

    fix: spacing

--- server/index.js
+++ server/index.js
@@ -4,4 +4,4 @@ const { app } = require('./app');
 
 const { PORT } = process.env;
 
-app.listen(PORT, () => console.info(`Server listening on : ${PORT}`));
+app.listen(PORT, () => console.info(`Server listening on: ${PORT}`));
