8a79a103a8a927a89b22238f3ab4e873f9207e48
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-02-08 11:01:45 -0400

    refactor: removed variable name

--- server/app.js
+++ server/app.js
@@ -3,11 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const CLIENT_PATH = path.resolve(__dirname, '../build');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(CLIENT_PATH));
+app.use(express.static(path.resolve(__dirname, '../build')));
 app.use(cors());
 
 module.exports = {
