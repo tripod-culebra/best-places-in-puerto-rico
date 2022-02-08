3fa8ef2e6e094d54a1acf459ab79aac2b09fae2c
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-02-08 13:41:11 -0400

    fix: drop un-used api request code

--- server/app.js
+++ server/app.js
@@ -1,13 +1,8 @@
 const path = require('path');
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, '../build')));
-app.use(cors());
 
 module.exports = {
     app,
