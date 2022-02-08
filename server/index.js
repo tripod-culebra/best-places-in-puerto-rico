18f2ad4c1869e0746992ec8b916a2f07cb59e3ca
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-02-08 13:42:39 -0400

    fix: spacing

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const dotenv = require('dotenv');
 dotenv.config({
     path: path.resolve(__dirname, '../.env'),
 });
+
 const { app } = require('./app');
 
 const PORT = process.env.PORT || 4000;
+
 app.listen(PORT, () => console.info(`Server listening on :${PORT}`));
