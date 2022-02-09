ee9d1f8c9ec589c0e97dd4460a9cd7dff84de417
ottonotto <84978723+ottonotto@users.noreply.github.com>
2022-02-09 15:41:43 -0400

    fix: add build routing

--- server/app.js
+++ server/app.js
@@ -1,7 +1,18 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 
 const app = express();
-app.use(express.static(path.resolve(__dirname, '../build')));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, '../build')));
+}
+app.use('/api/*', (req, res) => {
+    res.send([]);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 
 module.exports = { app };
